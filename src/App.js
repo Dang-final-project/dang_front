9539be3280603d9eb675dd81911ac4b30f69825e
yerim <a123dpfla51@gmail.com>
2024-05-09 10:29:22 +0900

    popup 초기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import "./App.css";
-import BasicPopup from "./components/popup/BasicPopup";
-import ContentsPopup from "./components/popup/ContentsPopup";
-import SelectPopup from "./components/popup/SelectPopup";
 
 function App() {
-    return (
-        <div>
-            <BasicPopup>예약</BasicPopup>
-            <ContentsPopup></ContentsPopup>
-            <SelectPopup title="예약하시겠습니까?">
-                <textarea></textarea>
-            </SelectPopup>
-        </div>
-    );
+    return <div></div>;
 }
 
 export default App;
