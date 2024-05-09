173eb3d5ff535b271d8d7527444ec5236c741ba2
kueunsu <sss5732@naver.com>
2024-05-09 10:42:21 +0900

    input_text_components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { TextArea } from './components/text_input/TextArea';
+import { TextInput } from './components/text_input/TextInput';
 
 function App() {
   return (
     <div>
-      
+
+      <TextArea />
+      <TextInput />
     </div>
   );
 }
