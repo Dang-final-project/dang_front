66dffa55b82afb860604757b88cd2df8ef215a13
yerim <a123dpfla51@gmail.com>
2024-05-09 10:26:03 +0900

    popup 초기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BasicPopup from "./components/popup/BasicPopup";
+import ContentsPopup from "./components/popup/ContentsPopup";
+import SelectPopup from "./components/popup/SelectPopup";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <BasicPopup>예약</BasicPopup>
+            <ContentsPopup></ContentsPopup>
+            <SelectPopup title="예약하시겠습니까?">
+                <textarea></textarea>
+            </SelectPopup>
+        </div>
+    );
 }
 
 export default App;
