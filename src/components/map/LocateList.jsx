8ee49e8480e5a9c9207bbfb351b4527dcc24e9f4
gigajini <bh5322@naver.com>
2024-05-10 17:08:37 +0900

    충전소리스트 api 연결

--- src/components/map/LocateList.jsx
+++ src/components/map/LocateList.jsx
@@ -2,8 +2,9 @@ import { Box, Paper, Tabs, Tab, Typography } from "@mui/material";
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Station from "./Station";
+import axios from "axios";
 
 
 const LocateList = () => {
@@ -13,6 +14,30 @@ const LocateList = () => {
     const handleChange = (event, newValue) => {
       setValue(newValue);
     };
+
+    const [stations, setStations] = useState([]);
+
+    const getStations = async() => {
+        try{
+            const key = 'KE8fpP1J%2B89PviF5ypn1iC2Pt13cnUqW7zS6rTyC01AY5TnWK7Ke2zgCzNUU8TF3zQyZiEr6YfRfclI79xarRg%3D%3D';
+            const pageIdx = 1;
+            const count = 10;
+            const url = `https://api.odcloud.kr/api/15067156/v1/uddi:4f293dcb-a55b-4f64-b7d1-dab5b0ba56bb?page=${pageIdx}&perPage=${count}&serviceKey=${key}`;
+            const response = await axios.get(url);
+            if(response.status === 200){
+                setStations(response.data.data);
+            }
+
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+    useEffect(()=>{
+        getStations();
+    },[])
+
+    console.log(stations);
 
     return ( 
         <Paper sx={{p:2, maxWidth:'460px', flexGrow:1, overflow:'hidden'}}>
