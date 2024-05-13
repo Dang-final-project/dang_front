307c5c6d389ee46f7c339857e47c8d5c7ca4f369
Hyom01 <hc0606@bu.ac.kr>
2024-05-13 12:55:20 +0900

    FilterList

---
+++ src/components/filter/Open.jsx
@@ -0,0 +1,79 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import { Grid } from "@mui/material";
+import { useState } from "react"; 
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    "& .MuiDialogContent-root": {
+        padding: theme.spacing(2),
+    },
+    "& .MuiDialogActions-root": {
+        padding: theme.spacing(1),
+    },
+}));
+
+export default function Open({ title, width = 500, height = 100, children }) {
+    const [open, setOpen] = React.useState(false);
+    const [selectedOpen, setSelectedOpen] = useState(''); 
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const dialogContentProps = {
+        sx: { width: width, height: height },
+        ...(title && { dividers: true }),
+    };
+
+    const handleOpenClick = (open) => {
+        setSelectedOpen(open); 
+    };
+
+    return (
+        <React.Fragment>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                외부인 개방
+            </Button>
+            <BootstrapDialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+                <DialogTitle sx={{ m: 0, p: 2, fontWeight: 600 }} id="customized-dialog-title">
+                    외부인 개방
+                </DialogTitle>
+                <IconButton
+                    aria-label="close"
+                    onClick={handleClose}
+                    sx={{
+                        position: "absolute",
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                    }}
+                >
+                    <CloseIcon />
+                </IconButton>
+                <DialogContent {...dialogContentProps}>
+                    <Grid container justifyContent="center">
+                        <Grid item>
+                            <Button
+                                variant={selectedOpen === '외부인 개방' ? 'contained' : 'outlined'} 
+                                size="large"
+                                color="primary"
+                                onClick={() => handleOpenClick('외부인 개방')}
+                            >
+                                외부인 개방
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+            </BootstrapDialog>
+        </React.Fragment>
+    );
+}
