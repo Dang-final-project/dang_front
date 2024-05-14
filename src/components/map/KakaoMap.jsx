5411d16403e6c0aae3cd65668d291e1e3d6ac6f8
Hyom01 <hc0606@bu.ac.kr>
2024-05-14 13:03:34 +0900

    clustermaker

--- src/components/map/KakaoMap.jsx
+++ src/components/map/KakaoMap.jsx
@@ -4,11 +4,32 @@ import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
 const KakaoMap = () => {
     const [loading, error] = useKakaoLoader({
         appkey: process.env.REACT_APP_KAKAO_MAP_API_KEY,
+        libraries: ["clusterer"]
     });
 
     const center = { lat: 33.5563, lng: 126.79581 };
 
     const [position, setPosition] = useState(center);
+
+    const onClusterclick = (_target, cluster) => {
+        const map = mapRef.current;
+        const level = map.getLevel() - 1;
+        map.setLevel(level, { anchor: cluster.getCenter() });
+    };
+
+    useEffect(() => {
+        // 마커 클러스터링에 사용할 위치 데이터 설정
+        const clusterPositionsData = {
+            positions: [
+                { title: "카카오", latlng: { lat: 33.450705, lng: 126.570677 } },
+                { title: "생태연못", latlng: { lat: 33.450936, lng: 126.569477 } },
+                { title: "텃밭", latlng: { lat: 33.450879, lng: 126.56994 } },
+                { title: "근린공원", latlng: { lat: 33.451393, lng: 126.570738 } },
+                // 추가적인 위치 데이터도 필요하다면 이곳에 추가
+            ]
+        };
+        setPositions(clusterPositionsData.positions);
+    }, []);
 
     return (
         <Map
