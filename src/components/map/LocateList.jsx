b2a921a3e85f44f121974ad7c5e1247696737e01
yerim <a123dpfla51@gmail.com>
2024-05-14 22:14:20 -0700

    search input 소문자도 대문자로 검색 가능하게 수정

--- src/components/map/LocateList.jsx
+++ src/components/map/LocateList.jsx
@@ -34,7 +34,7 @@ const LocateList = () => {
             const response = await axios.get(url);
             const datas = response.data.items;
             if (response.status === 200) {
-                const filteredStations = datas.filter((station) => station.chrstnNm.includes(searchWord));
+                const filteredStations = datas.filter((station) => station.chrstnNm.includes(searchWord.toUpperCase()));
                 setStations(filteredStations);
             }
         } catch (err) {
@@ -51,12 +51,10 @@ const LocateList = () => {
             const response = await axios.get(url);
             if(response.status === 200){
                 const results = [];
-                response.data.items.forEach(item => {
+                response.data.items.forEach((item) => {
                     const cur_lat = item.latitude;
                     const cur_lng = item.longtitude;
-                    const ex_item = results.find(
-                        r => r.latitude === cur_lat && r.longtitude === cur_lng
-                    );
+                    const ex_item = results.find((r) => r.latitude === cur_lat && r.longtitude === cur_lng);
                     if (!ex_item) {
                         //충전소 상태 체크(2는 사용중)
                         if (item.charger_status === "2") {
@@ -64,7 +62,7 @@ const LocateList = () => {
                         } else {
                             item.avail_count = 0;
                         }
-                        item.tot_count = 1
+                        item.tot_count = 1;
                         results.push(item);
                     } else {
                         if (item.charger_status === "2") {
@@ -82,12 +80,12 @@ const LocateList = () => {
     };
 
     //로그인 구현되면 api호출방식 get으로 변경해야함
-    const getFav = async () => {
-        const urll = `http://localhost:8000/v1/stations/list`;
-        const fav = await axios.post(urll, { "id": 2 });
-        //console.log(fav.data.payload);
-        setFavList(fav.data.payload);
-    }
+    // const getFav = async () => {
+    //     const urll = `http://localhost:8000/v1/stations/list`;
+    //     const fav = await axios.post(urll, { id: 2 });
+    //     console.log(fav.data.payload);
+    //     setFavList(fav.data.payload);
+    // };
 
     const getFavStations = async() => {
         const key = process.env.REACT_APP_STATION_API_KEY;
@@ -104,17 +102,15 @@ const LocateList = () => {
                     response.data.items.forEach(item => {
                         const cur_lat = item.latitude;
                         const cur_lng = item.longtitude;
-                        const ex_item = results.find(
-                            r => r.latitude === cur_lat && r.longtitude === cur_lng
-                        );
-                        if(!ex_item){
+                        const ex_item = results.find((r) => r.latitude === cur_lat && r.longtitude === cur_lng);
+                        if (!ex_item) {
                             if (item.charger_status === "2") {
                                 item.avail_count = 1;
                             } else {
                                 item.avail_count = 0;
                             }
-                            item.tot_count = 1
-                            results.push(item)
+                            item.tot_count = 1;
+                            results.push(item);
                         } else {
                             if (item.charger_status === "2") {
                                 ex_item.avail_count += 1;
@@ -129,16 +125,70 @@ const LocateList = () => {
                 console.error(err);
             }
         }
-    }
+    };
 
     useEffect(() => {
         getStations();
-        getFav();
-    },[])
+        // getFav();
+    }, []);
 
-    useEffect(()=>{
+    useEffect(() => {
         getFavStations();
-    },[favList])
+    }, [favList]);
+
+    return (
+        <>
+            <SearchBox onClick={handleSearch} handleSearchChange={handleSearchChange} />
+            <Paper sx={{ p: 2, maxWidth: "460px", flexGrow: 1, overflow: "hidden" }}>
+                <Typography>
+                    주변 충전소 : <span>{stations.length}</span>개
+                </Typography>
+                <TabContext value={value}>
+                    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                        <TabList onChange={handleChange} aria-label="충전소리스트">
+                            <Tab label="충전소 리스트" value="1" sx={{ width: "50%" }} />
+                            <Tab label="MY충전소" value="2" sx={{ width: "50%" }} />
+                        </TabList>
+                    </Box>
+                    <TabPanel value="1" sx={{ height: "100%", overflow: "scroll" }}>
+                        {stations ? (
+                            stations.map((station, idx) => {
+                                return (
+                                    <Station
+                                        key={idx}
+                                        station={station}
+                                        favList={favList}
+                                        // getFav={getFav}
+                                        avail_memo={false}
+                                    />
+                                );
+                            })
+                        ) : (
+                            <Typography>데이터 로딩중</Typography>
+                        )}
+                    </TabPanel>
+                    <TabPanel value="2" sx={{ height: "100%", overflow: "scroll" }}>
+                        {favStation && favList.length !== 0 ? (
+                            favStation.map((fav, idx) => {
+                                return (
+                                    <Station
+                                        key={idx}
+                                        station={fav}
+                                        favList={favList}
+                                        // getFav={getFav}
+                                        avail_memo={true}
+                                    />
+                                );
+                            })
+                        ) : (
+                            <Typography>즐겨찾기가 존재하지 않습니다.</Typography>
+                        )}
+                    </TabPanel>
+                </TabContext>
+            </Paper>
+        </>
+    );
+};
 
     return ( 
         <Paper sx={{p:2, maxWidth:'460px', flexGrow:1, overflow:'hidden'}}>
