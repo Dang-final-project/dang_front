d67cfb07773a8c2728838e6c02ccc283adbcd091
hokyunghwa <159886750+hokyunghwa@users.noreply.github.com>
2024-05-16 01:30:38 +0900

    cartype초안

---
+++ src/components/CarType.jsx
@@ -0,0 +1,41 @@
+import { Grid, Box, Button, Typography } from '@mui/material';
+import SearchInput from './input/SearchInput';
+
+const CarType = () => {
+
+    return ( 
+        <Grid sx={{display: "flex", flexDirection: "column", alignItems: "center",  }}>
+        <Box 
+            sx={{ 
+                backgroundColor: '#f1f1f1', 
+                width: '60%', 
+                border: '1px solid black',
+                boxShadow: '0px 5px 5px rgba(58, 56, 56, 0.5)',
+                padding: '2vh'
+                }}>
+            <Typography>제조사별</Typography>
+            <Button variant="outlined" sx={{marginRight: '1vh'}}>현대</Button>
+            <Button variant="outlined">기아</Button>
+            <Typography sx={{marginTop: '1vh'}}>승차인원</Typography>
+            <Button variant="outlined" sx={{marginRight: '1vh'}}>4인승</Button>
+            <Button variant="outlined">5인승</Button>
+            <Typography sx={{marginTop: '1vh'}}>충전방식</Typography>
+            <Button variant="outlined" sx={{marginRight: '1vh'}}>DC차데모<br/>(급속)</Button>
+            <Button variant="outlined">B타입5핀<br/>(완속)</Button>
+            <Typography sx={{marginTop: '1vh'}}>차량검색</Typography>
+            <SearchInput />
+        </Box>
+        <Box
+            sx={{
+                marginTop: '6vh',
+                width: '50%',
+                height: '20vh',
+                border: '1px solid black'
+            }}>
+        차정보
+        </Box>
+        </Grid>
+    );
+}
+
+export default CarType;
