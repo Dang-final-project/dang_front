fdd3bf0e890e4df0d314e19d92a6dd447325bbaf
yerim <a123dpfla51@gmail.com>
2024-05-17 05:13:53 -0700

    로그인시 헤더 변경과 비매너 차량 신고 구현

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,117 +1,148 @@
 import { useTheme } from "@emotion/react";
-import {
-    Box,
-    Button,
-    FormControl,
-    Grid,
-    InputLabel,
-    OutlinedInput,
-    TextField,
-    Typography,
-    useMediaQuery,
-} from "@mui/material";
-import { useState } from "react";
+import { Box, Button, FormControl, Grid, OutlinedInput, TextField, Typography, useMediaQuery } from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useAuth } from "../hooks/useAuth";
 
-const Community = () => {
-    const [activeButton, setActiveButton] = useState("review");
+const Report = () => {
+    const { loginUser } = useAuth();
+    const [carNum, setCarNum] = useState("");
+    const [station, setStation] = useState("");
+    const [content, setContent] = useState("");
     const theme = useTheme();
     const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
 
-    const Report = () => {
-        const handleSubmit = () => {
-            alert("완료!");
-        };
-        return (
-            <>
-                <Typography variant="h5" sx={{ margin: "30px" }}>
-                    비매너 차량 신고
-                </Typography>
-                <form action="#" onSubmit={handleSubmit}>
-                    {isDesktop ? (
-                        <>
-                            <Grid
-                                sx={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    alignItems: "flex-end",
-                                    marginRight: "60px",
-                                }}
-                            >
-                                <FormControl
-                                    sx={{ mb: 2, display: "flex", flexDirection: "row", alignItems: "center" }}
-                                >
-                                    <Typography sx={{ marginRight: 2 }}>차번호 :</Typography>
-                                    <OutlinedInput sx={{ minWidth: "400px" }}></OutlinedInput>
-                                </FormControl>
-                                <FormControl
-                                    sx={{ mb: 2, display: "flex", flexDirection: "row", alignItems: "center" }}
-                                >
-                                    <Typography sx={{ marginRight: 2 }}>충전소 :</Typography>
-                                    <OutlinedInput sx={{ minWidth: "400px" }}></OutlinedInput>
-                                </FormControl>
-                                <FormControl
-                                    sx={{ mb: 2, display: "flex", flexDirection: "row", alignItems: "center" }}
-                                >
-                                    <Typography sx={{ marginRight: 2, whiteSpace: "nowrap" }}>신고 내용 :</Typography>
-                                    <TextField multiline rows={9} sx={{ minWidth: "400px" }}></TextField>
-                                </FormControl>
-                                <Button
-                                    type="submit"
-                                    variant="contained"
-                                    sx={{ width: "300px", height: "40px", marginRight: "60px", marginBottom: "20px" }}
-                                >
-                                    신고
-                                </Button>
-                            </Grid>
-                        </>
-                    ) : (
-                        <>
-                            <Grid
-                                sx={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    alignItems: "center",
-                                }}
-                            >
-                                <FormControl
-                                    sx={{
-                                        mb: 2,
-                                        display: "flex",
-                                        flexDirection: "column",
-                                        alignItems: "left",
-                                    }}
-                                >
-                                    <Typography sx={{ marginRight: 2, marginBottom: "10px" }}>차번호</Typography>
-                                    <OutlinedInput sx={{ minWidth: "400px" }}></OutlinedInput>
-                                </FormControl>
-                                <FormControl
-                                    sx={{ mb: 2, display: "flex", flexDirection: "column", alignItems: "left" }}
-                                >
-                                    <Typography sx={{ marginRight: 2, marginBottom: "10px" }}>충전소</Typography>
-                                    <OutlinedInput sx={{ minWidth: "400px" }}></OutlinedInput>
-                                </FormControl>
-                                <FormControl
-                                    sx={{ mb: 2, display: "flex", flexDirection: "column", alignItems: "left" }}
-                                >
-                                    <Typography sx={{ marginRight: 2, marginBottom: "10px", whiteSpace: "nowrap" }}>
-                                        신고 내용
-                                    </Typography>
-                                    <TextField multiline rows={9} sx={{ minWidth: "400px" }}></TextField>
-                                </FormControl>
-                                <Button
-                                    type="submit"
-                                    variant="contained"
-                                    sx={{ width: "200px", height: "40px", marginBottom: "20px" }}
-                                >
-                                    신고
-                                </Button>
-                            </Grid>
-                        </>
-                    )}
-                </form>
-            </>
-        );
+    const handleSubmit = async (e) => {
+        const UserId = loginUser?.id;
+        e.preventDefault();
+        try {
+            const res = await axios.post("http://localhost:8000/v1/community/report", {
+                carNum,
+                station,
+                content,
+                UserId,
+            });
+            if (res.data.code === 200) {
+                Swal.fire({
+                    title: `신고 되었습니다`,
+                    icon: "success",
+                    confirmButtonText: "확인",
+                    confirmButtonColor: theme.palette.primary.main,
+                });
+                setCarNum("");
+                setStation("");
+                setContent("");
+            }
+        } catch (err) {
+            console.error(err);
+        }
     };
+
+    const handleCarNumChange = useCallback((e) => {
+        setCarNum(e.target.value);
+    }, []);
+
+    const handleStationChange = useCallback((e) => {
+        setStation(e.target.value);
+    }, []);
+
+    const handleContentChange = useCallback((e) => {
+        setContent(e.target.value);
+    }, []);
+
+    return (
+        <>
+            <Typography variant="h5" sx={{ margin: "30px" }}>
+                비매너 차량 신고
+            </Typography>
+            <form onSubmit={handleSubmit}>
+                <Grid
+                    sx={{
+                        display: "flex",
+                        flexDirection: isDesktop ? "column" : "column",
+                        alignItems: isDesktop ? "flex-end" : "center",
+                        marginRight: isDesktop ? "60px" : "0",
+                    }}
+                >
+                    <FormControl
+                        sx={{
+                            mb: 2,
+                            display: "flex",
+                            flexDirection: isDesktop ? "row" : "column",
+                            alignItems: isDesktop ? "center" : "left",
+                        }}
+                    >
+                        <Typography sx={{ marginRight: 2 }}>차번호 {isDesktop && ":"}</Typography>
+                        <OutlinedInput
+                            name="carNum"
+                            value={carNum}
+                            onChange={handleCarNumChange}
+                            sx={{ minWidth: "400px" }}
+                        />
+                    </FormControl>
+                    <FormControl
+                        sx={{
+                            mb: 2,
+                            display: "flex",
+                            flexDirection: isDesktop ? "row" : "column",
+                            alignItems: isDesktop ? "center" : "left",
+                        }}
+                    >
+                        <Typography sx={{ marginRight: 2 }}>충전소 {isDesktop && ":"}</Typography>
+                        <OutlinedInput
+                            name="station"
+                            value={station}
+                            onChange={handleStationChange}
+                            sx={{ minWidth: "400px" }}
+                        />
+                    </FormControl>
+                    <FormControl
+                        sx={{
+                            mb: 2,
+                            display: "flex",
+                            flexDirection: isDesktop ? "row" : "column",
+                            alignItems: isDesktop ? "center" : "left",
+                        }}
+                    >
+                        <Typography sx={{ marginRight: 2, whiteSpace: "nowrap" }}>
+                            신고 내용 {isDesktop && ":"}
+                        </Typography>
+                        <TextField
+                            name="content"
+                            value={content}
+                            onChange={handleContentChange}
+                            multiline
+                            rows={9}
+                            sx={{ minWidth: "400px" }}
+                        />
+                    </FormControl>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{
+                            width: isDesktop ? "300px" : "200px",
+                            height: "40px",
+                            marginRight: isDesktop ? "60px" : "0",
+                            marginBottom: "20px",
+                        }}
+                    >
+                        신고
+                    </Button>
+                </Grid>
+            </form>
+        </>
+    );
+};
+
+const Community = () => {
+    const [activeButton, setActiveButton] = useState("review");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const { loginUser } = useAuth();
+
+    useEffect(() => {
+        loginUser?.id ? setIsLoggedIn(true) : setIsLoggedIn(false);
+    }, [loginUser]);
 
     return (
         <Grid sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
@@ -136,19 +167,17 @@ const Community = () => {
                         backgroundColor: activeButton === "report" ? "primary.main" : "primary.light",
                     }}
                     onClick={() => {
-                        setActiveButton("report");
+                        isLoggedIn ? setActiveButton("report") : window.location.replace("/login");
                     }}
                 >
                     신고하기
                 </Button>
             </Box>
             {activeButton === "review" ? (
-                // 여기 추가해주시면 됩니다!
+                //이 부분에 추가해주시면 됩니다!
                 "리뷰"
             ) : (
-                <>
-                    <Report />
-                </>
+                <Report />
             )}
         </Grid>
     );
