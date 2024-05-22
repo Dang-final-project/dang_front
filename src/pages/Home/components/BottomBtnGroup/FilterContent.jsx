9a6042182535842403ea0da460b5bc27e185c89a
gigajini <bh5322@naver.com>
2024-05-22 10:21:11 +0900

    홈화면 기능 분리

--- src/pages/Home/components/BottomBtnGroup/FilterContent.jsx
+++ src/pages/Home/components/BottomBtnGroup/FilterContent.jsx
@@ -1,41 +1,47 @@
 import { Stack, Typography, Box, Button } from "@mui/material";
 import { FilterValue } from "../FilterGroup/FilterValue";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const FilterContent = () => {
 
-    //const [btnClicked, setBtnClicked] = useState(false);
+    // 기능 개발 미완성
+    const [filterButtons, setFilterButtons] = useState();
+    // const test = (fk) => {
+    //     console.log(filter[fk])
+    //     setFilter({...filter, [fk]: !filter[fk]})
+    // }
+    // const test2 = (fk, v) => {
+    //     setFilter({...filter, [fk]: v})
+    // }
 
-    console.log(FilterValue[3].filterValue.length)
-
-    const [filterData, setFilterData] = useState();
-
-    //단일버튼
-    const selectSingleBtn = (index) => {
-        const { filterKey, filterValue } = FilterValue[index];
-        
-    }
-
-    //복합버튼
+    console.log(filterButtons);
 
     return ( 
         <Stack spacing={2}>
             {
-                FilterValue.map((filter,idx) => {
+                FilterValue.map((f,idx) => {
                     return (
                         <Box key={idx}>
                             <Box sx={{display:'flex', gap:'4px', color:"grey.700"}}>
-                                {filter.icon}
-                                <Typography>{filter.label}</Typography>
+                                {f.icon}
+                                <Typography>{f.label}</Typography>
                             </Box>
                             <Stack direction="row" spacing={1}>
+                                {/* {
+                                    f.needPopup ?
+                                    f.filterValue.map((value,idx)=>(
+                                        <Button key={idx} variant={filter[f.filterKey]==null ?"contained" : "outlined"} onClick={()=> test2(f.filterKey, value)}>{value}</Button>
+                                    ))
+                                    :
+                                    <Button key={idx} variant={filter[f.filterKey] ? "contained" : "outlined"} onClick={()=> test(f.filterKey)}>{f.label}</Button>
+                                } */}
                                 {
-                                    filter.needPopup ?
-                                    filter.filterValue.map((value,idx)=>(
+                                    f.needPopup ?
+                                    f.filterValue.map((value,idx)=>(
                                         <Button key={idx} variant="outlined">{value}</Button>
                                     ))
                                     :
-                                    <Button key={idx} variant="outlined" onClick={selectSingleBtn(idx)}>{filter.label}</Button>
+                                    <Button key={idx} variant="outlined">{f.label}</Button>
                                 }
                             </Stack>
                         </Box>
