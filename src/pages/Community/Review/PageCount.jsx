b6e0cfd6dc434c165d02726bd92f0ef3c0c19ff8
unknown <hc0606@gmail.com>
2024-05-23 10:32:19 +0900

    pagenation_review

---
+++ src/pages/Community/Review/PageCount.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Grid, Pagination, Stack } from "@mui/material";
+
+const PageCount = ({ page, count, handleChangePage }) => {
+    return (
+        <Grid sx={{ marginBottom: "100px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Stack spacing={3}>
+                <Pagination
+                    onChange={handleChangePage}
+                    count={count}
+                    page={page}
+                    variant="outlined"
+                    shape="rounded"
+                />
+            </Stack>
+        </Grid>
+    );
+};
+
+export default PageCount;
--- src/pages/Community/Review/Review.jsx
+++ src/pages/Community/Review/Review.jsx
@@ -7,12 +7,14 @@ import Typography from '@mui/material/Typography';
 import { Box, Button } from "@mui/material";
 import axios from "axios";
 import {useAuth} from "../../../hooks/useAuth";
-
+import PageCount from "./PageCount";
 
 const Review = () => {
     const [reviews, setReviews] = useState([]); // 초기값을 빈 배열로 설정
+    const [page, setPage] = useState(1);
+    const reviewsPerPage = 3;
     const navigate = useNavigate();
-    const { loginUser, logout } = useAuth();
+    const { loginUser } = useAuth();
 
     const getReviews = async () => {
         try {
@@ -22,17 +24,22 @@ const Review = () => {
                 }
             });
             if (res.data.code === 200) {
-                setReviews(res.data.payload || null);
+                setReviews(res.data.payload || []);
             }
         } catch (error) {
             console.error("Error fetching reviews:", error);
         }
     };
-    
 
     useEffect(() => {
         getReviews();
     }, []);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const currentData = reviews.slice((page - 1) * reviewsPerPage, page * reviewsPerPage);
 
     const handleWriteButtonClick = () => {
         navigate("/community/posting");
@@ -42,17 +49,19 @@ const Review = () => {
         <>
             <SearchInput width='100%' />
             <List sx={{ width: '100%', maxWidth: 360, }}>
-                {reviews.length > 0 ? (
-                    reviews.map((review, index) => (
+                {currentData.length > 0 ? (
+                    currentData.map((review, index) => (
                         <ListItem key={index} sx={{ flexDirection: "column" }}>
-                            <Box sx={{ display: "flex", alignContent: "center", justifyContent: "space-between", width: '100%' }}>
-                                <Typography variant="body1">{review.station}</Typography>
-                                <Typography variant="body1">
-                                    작성자: {review.User.username}
-                                </Typography>
-                                <Typography variant="body1">
-                                    작성일: {new Date(review.created_at).toLocaleDateString()}
-                                </Typography>
+                            <Box sx={{ display: "inline-block", alignContent: "center", justifyContent: "space-between", width: '100%' }}>
+                                <Typography variant="h6">{review.station}</Typography>
+                                <Box sx={{display: "flex", justifyContent: "space-between"}}>
+                                    <Typography variant="body2">
+                                        작성자: {review.User.username}
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        작성일: {new Date(review.createdAt).toLocaleString()}
+                                    </Typography>
+                                </Box>
                             </Box>
                             <Box sx={{ display: "flex", flexDirection: "column", alignContent: "center", justifyContent: "flex-start", width: '100%' }}>
                                 <Typography variant="body1" component="span">
@@ -65,12 +74,24 @@ const Review = () => {
                         </ListItem>
                     ))
                 ) : (
-                    <Typography variant="body1">리뷰가 없습니다.</Typography>
+                    <Typography variant="body1">리뷰를 작성해주세요.</Typography>
                 )}
             </List>
-            <Button variant="text" onClick={handleWriteButtonClick}>작성하기</Button>
+            <PageCount
+                page={page}
+                count={Math.ceil(reviews.length / reviewsPerPage)}
+                handleChangePage={handleChangePage}
+                sx={{ marginBottom: "20px" }}
+            />
+            <Button
+                variant="text"
+                onClick={handleWriteButtonClick}
+                sx={{ marginBottom: "50px", marginTop: "-20px" }}
+            >
+                작성하기
+            </Button>
         </>
     );
-}
+};
 
 export default Review;
