3393d2500eebf1a754969e6e8547512b46904226
unknown <hc0606@gmail.com>
2024-05-24 16:08:13 +0900

    map

--- src/pages/Home/components/Map/KakaoMap.jsx
+++ src/pages/Home/components/Map/KakaoMap.jsx
@@ -1,12 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { Map, useKakaoLoader } from 'react-kakao-maps-sdk';
 import { MapContext } from '../../../../contexts/MapContext'; // 경로 수정
 import Geolocate from './Geolocate';
 import ClusterMarker from './ClusterMarker';
-import { Margin } from '@mui/icons-material';
 
 const KakaoMap = () => {
   const { positionArr, stations, mapPos, setMapPos } = useContext(MapContext);
+  
   //console.log(stations);
 
   const [loading, error] = useKakaoLoader({
@@ -14,11 +14,28 @@ const KakaoMap = () => {
     libraries: ["clusterer"],
   });
 
+  const mapRef = useRef(); // map 참조 생성
   const [center, setCenter] = useState({
     lat: 33.450701,
     lng: 126.570667,
   });
   const [position, setPosition] = useState(center);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(pos => {
+        setCenter({lat:pos.coords.latitude, lng:pos.coords.longitude});
+    })
+    navigator.geolocation.watchPosition(pos => {
+        setPosition({lat:pos.coords.latitude, lng:pos.coords.longitude});
+    });
+}, []);
+
+const onCenterChanged = (map) => {
+  setCenter({
+    lat: map.getCenter().getLat(),
+    lng: map.getCenter().getLng(),
+  });
+};
     
   useEffect(() => {
       setCenter(mapPos);
@@ -33,8 +50,10 @@ const KakaoMap = () => {
       center={center}
       style={{ width: "100%", height: "calc(100vh - 64px - 58.5px)" }}
       level={3}
+      ref={mapRef}
+      onCenterChanged={onCenterChanged}
     >
-      <Geolocate center={center} position={position} setCenter={setCenter} setPosition={setPosition} 
+      <Geolocate center={center} position={position} setCenter={setCenter} setPosition={setPosition} mapRef={mapRef}
         sx={{marginbottom: "100%"}}
       />
       <ClusterMarker />
