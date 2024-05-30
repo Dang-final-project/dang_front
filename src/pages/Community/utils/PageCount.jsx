8b23eb71fdc4d2c0d770f05e5692076c0d6393eb
unknown <hc0606@gmail.com>
2024-05-30 11:06:26 +0900

    infinitescroll

--- src/pages/Community/utils/PageCount.jsx
+++ src/pages/Community/utils/PageCount.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Grid, Pagination, Stack } from "@mui/material";
 
 const PageCount = ({ page, count, handleChangePage }) => {
+    console.log(page, count);
     return (
         <Grid sx={{ marginBottom: "100px", display: "flex", flexDirection: "column", alignItems: "center" }}>
             <Stack spacing={3}>
                 <Pagination
-                    onChange={handleChangePage}
+                    onChange={(e) => handleChangePage(e)}
                     count={count}
-                    page={page}
+                    page={parseInt(page)}
                     variant="outlined"
                     shape="rounded"
                 />
